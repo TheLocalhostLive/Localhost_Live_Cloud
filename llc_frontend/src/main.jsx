739434c845aef096a7064f8e61870b06f45991b2
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2024-09-28 18:36:10 +0530

    stashed changes included.

--- llc_frontend/src/main.jsx
+++ llc_frontend/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <Auth0Provider
+    domain="dev-1yffugckd6d5gydc.us.auth0.com"
+    clientId="W9yLUPon9XtgpquZKmURQj4URCRthWS1"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
     <App />
-  </StrictMode>,
-)
+  </Auth0Provider>
+);
