178e38dae1d8851444c0adc0910a91cf57214ac9
RedAnt-Tonmay <tonmaysardar500@gmail.com>
2024-09-30 12:46:39 +0530

    Check Console page : Added

---
+++ llc_frontend/src/components/ConsolePage.jsx
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import React, { useEffect } from 'react'
+import { useState } from 'react';
+import { useLocation , useParams} from 'react-router-dom'
+export default function ConsolePage() {
+    const {state }= useLocation()
+    const {container_name} = state || "";
+    const [logs, updateLogs] = useState("");
+    useEffect(()=>{
+        axios.get(`http://localhost:8080/process/${container_name}`)
+      .then((res) => {
+        updateLogs(res.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching deployed projects", error);
+      });
+    },[])
+    
+  return (
+    <>
+     <h1> Console For :{container_name}</h1>
+        <dev className ="">
+            {logs}
+        </dev>
+    </>
+  )
+}
+ 
