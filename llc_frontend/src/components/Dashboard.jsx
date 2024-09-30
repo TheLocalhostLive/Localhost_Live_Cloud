087c237cd0960c1968eb9a7fd64fffdcad161c92
RedAnt-Tonmay <tonmaysardar500@gmail.com>
2024-09-30 15:49:12 +0530

    Dashboard Page Style Added

--- llc_frontend/src/components/Dashboard.jsx
+++ llc_frontend/src/components/Dashboard.jsx
@@ -1,15 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import '../style/dashboard.css';
+import React, { useEffect, useState } from "react";
+import "../style/dashboard.css";
 import { AiOutlinePlus, AiOutlineCheck, AiOutlineCode } from "react-icons/ai";
 import axios from "axios";
+import Button from "@mui/joy/Button";
 
-import { useNavigate } from 'react-router-dom';
+import { AiFillHeart } from "react-icons/ai";
+
+import { useNavigate } from "react-router-dom";
+import BasicChips from "./ui_components/BasicChips";
 
 function Dashboard() {
   const [DeployedList, updateDeployedList] = useState([]);
   const navigate = useNavigate();
   useEffect(() => {
-    axios.get("http://localhost:8080/deploy")
+    axios
+      .get("http://localhost:8080/deploy")
       .then((res) => {
         updateDeployedList(res.data);
       })
@@ -18,54 +23,89 @@ function Dashboard() {
       });
   }, []);
 
-
-  const handleCheckConsoleClick = (id,container_name) => {
-      // console.log(`Card with ID ${id} clicked`);
-      // console.log(container_name)
-      navigate(`/check-console`,{state:{container_name}});
+  const handleCheckConsoleClick = (id, container_name) => {
+    // console.log(`Card with ID ${id} clicked`);
+    // console.log(container_name)
+    navigate(`/check-console`, { state: { container_name } });
   };
 
   return (
     <>
-      <div className='top-container'>
-        <h1 className='top-container-h1'>Dashboard</h1>
-        <div className='top-container-div'>
-      
-          <button className='top-container-button'>
+      <div className="top-container">
+        <h1 className="top-container-h1">Dashboard</h1>
+        <div className="top-container-div">
+          <Button
+            color="warning"
+            onClick={function () {}}
+            variant="outlined"
+            sx={{
+              color: "rgb(255,215,0)",
+              display: "flex", // Ensure flexbox is applied to align items
+              alignItems: "center", // Align icon and text vertically
+              gap: "5px", // Space between the icon and text
+              "&:hover": {
+                color: "rgb(218,165,32)", // Adjust the text color on hover
+              },
+            }}
+          >
             <AiOutlinePlus />
             Deploy Project
-          </button>
+          </Button>
+          <Button
+            color="warning"
+            onClick={function () {}}
+            variant="outlined"
+            sx={{
+              color: "rgb(255,215,0)",
+              display: "flex", // Ensure flexbox is applied to align items
+              alignItems: "center", // Align icon and text vertically
+              gap: "5px", // Space between the icon and text
+              "&:hover": {
+                color: "rgb(218,165,32)", // Adjust the text color on hover
+              },
+            }}
+          >
+            <AiFillHeart />
+            Donate
+          </Button>
         </div>
       </div>
 
-     
-        <div className='container-outer'>
-           <label className='container-inner'>
-            <AiOutlineCheck/>
-              Running Projects
-           </label>
-        </div>
-    
-
-        <div className="deployed-list-container">
-  {DeployedList.map((post) => {
-    const { _id, application_name, container_name,tech } = post;
-    return (
-      <div key={_id} className="deployed-item">
-        <div>
-          <h2>{application_name}</h2>
-          <h3>{tech}</h3>
-        </div>
-        <button className="check-console-button"
-        onClick={() => handleCheckConsoleClick(_id, container_name)}
-        >
-          <AiOutlineCode />
-          Check Console
-        </button>
+      <div className="container-outer">
+        <BasicChips />
       </div>
-    );
-  })}
-</div>
+
+      <div className="deployed-list-container">
+        {DeployedList.map((post) => {
+          const { _id, application_name, container_name, tech } = post;
+          return (
+            <div key={_id} className="deployed-item">
+              <div>
+                <h2>{application_name}</h2>
+                <h3>{tech}</h3>
+              </div>
+
+              <Button
+                color="danger"
+                onClick={() => handleCheckConsoleClick(_id, container_name)}
+                variant="outlined"
+                sx={{
+                  color: "danger",
+                  display: "flex", // Ensure flexbox is applied to align items
+                  alignItems: "center", // Align icon and text vertically
+                  gap: "5px", // Space between the icon and text
+                  "&:hover": {
+                    color: "rgb(218,165,32)", // Adjust the text color on hover
+                  },
+                }}
+              >
+                <AiOutlineCode />
+                Donate
+              </Button>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 }
