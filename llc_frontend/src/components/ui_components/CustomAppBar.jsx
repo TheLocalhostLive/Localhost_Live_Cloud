2a37aa40767dbc65607ffff95c9d0e6b0d8e61fc
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2024-09-30 22:07:36 +0530

    CustomAppbar: Create separte component.

---
+++ llc_frontend/src/components/ui_components/CustomAppBar.jsx
@@ -0,0 +1,101 @@
+import { useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Button,
+  Typography,
+  IconButton,
+  Avatar,
+  Menu,
+  MenuItem,
+} from "@mui/material";
+import { useAuth0 } from "@auth0/auth0-react";
+
+export default function CustomAppBar() {
+  const {
+    user,
+    loginWithRedirect,
+    logout,
+  } = useAuth0();
+  const settings = [
+    {
+      name: "Profile",
+      callback: () => {
+        console.log("profile");
+      },
+    },
+    {
+      name: "Dashboard",
+      callback: () => {
+        console.log("profile");
+      },
+    },
+    {
+      name: "Logout",
+      callback: () => {
+        logout({ logoutParams: { returnTo: window.location.origin } });
+      },
+    },
+  ];
+
+  const [anchorElUser, setAnchorElUser] = useState(null);
+
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = (cb) => {
+    cb();
+    setAnchorElUser(null);
+  };
+  return (
+    <AppBar position="static" style={{ backgroundColor: "#3f51b5" }}>
+      <Toolbar>
+        <Typography variant="h6" style={{ flexGrow: 1 }}>
+          MyApp
+        </Typography>
+        {!user ? (
+          <Button
+            color="inherit"
+            variant="outlined"
+            onClick={loginWithRedirect}
+          >
+            Sign Up
+          </Button>
+        ) : (
+          <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+            <Avatar alt="Remy Sharp" src={`${user.picture}`} />
+          </IconButton>
+        )}
+        <Menu
+          sx={{ mt: "45px" }}
+          id="menu-appbar"
+          anchorEl={anchorElUser}
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          open={Boolean(anchorElUser)}
+          onClose={handleCloseUserMenu}
+        >
+          {settings.map((setting) => (
+            <MenuItem
+              id={setting.name}
+              key={setting.name}
+              onClick={() => handleCloseUserMenu(setting.callback)}
+            >
+              <Typography sx={{ textAlign: "center" }}>
+                {setting.name}
+              </Typography>
+            </MenuItem>
+          ))}
+        </Menu>
+      </Toolbar>
+    </AppBar>
+  );
+}
