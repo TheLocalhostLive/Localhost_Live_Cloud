ffbc05a889731a1cf654f9c362a8687b1c9204a2
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2024-10-07 02:39:16 +0530

    VirtualMachine: Add auth header.

--- llc_frontend/src/components/VirtulMatchine.jsx
+++ llc_frontend/src/components/VirtulMatchine.jsx
@@ -1,53 +1,63 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useLocation } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const VirtulMatchine = () => {
-    const { state } = useLocation();
-    const { container_name } = state || "";
-    const [public_url, updatePublic_url] = useState("");
-    const { user, getAccessTokenSilently } = useAuth0();
-    useEffect(() => {
-    
+  const { state } = useLocation();
+  const { container_name } = state || "";
+  const [public_url, updatePublic_url] = useState("");
+  const { user, getAccessTokenSilently } = useAuth0();
+  useEffect(() => {
+    getAccessTokenSilently()
+      .then((accessToken) => {
         let owner = user?.nickname;
-        if(!owner){
-            alert("You Must Logging")
-            return
+        if (!owner) {
+          alert("You Must Logging");
+          return;
         }
         let payload = {
-            owner,
-            container_name
-        }
-        
-        axios.get(`http://localhost:8080/launch/`,
-            {params:{...payload}}
-        )
-            .then((res) => {
-                updatePublic_url(res.data.public_url);
-                // Assuming the response contains the public URL
-            })
-            .catch((error) => {
-                console.error("Error fetching deployed projects", error);
-            });
-    }, [container_name]); // Depend on container_name
+          owner,
+          container_name,
+        };
 
-    // Navigate to the public URL when it's updated
-    useEffect(() => {
-        if (public_url) {
-            //console.log(public_url)
-            //console.log(`https://${public_url}`)
-            window.location.href = "https://"+public_url; // Redirect to the public URL
-        }
-    }, [public_url]);
+        axios
+          .get(
+            `http://localhost:8080/launch/`,
 
-    return (
-        <div>
-            {/* You can add additional UI elements here if needed */}
-            <h1>Launching Virtual Machine...</h1>
-            <h2> It May take some time</h2>
+            {
+              params: { ...payload },
+              headers: {
+                Authorization: `Bearer ${accessToken}`,
+                "Content-Type": "application/json",
+              },
+            }
+          )
+          .then((res) => {
+            updatePublic_url(res.data.public_url);
+          })
+          .catch((error) => {
+            console.error("Error fetching deployed projects", error);
+          });
+      });
+  }, [container_name]); // Depend on container_name
 
-            {/* {public_url&&
+  // Navigate to the public URL when it's updated
+  useEffect(() => {
+    if (public_url) {
+      //console.log(public_url)
+      //console.log(`https://${public_url}`)
+      window.location.href = "https://" + public_url; // Redirect to the public URL
+    }
+  }, [public_url]);
+
+  return (
+    <div>
+      {/* You can add additional UI elements here if needed */}
+      <h1>Launching Virtual Machine...</h1>
+      <h2> It May take some time</h2>
+
+      {/* {public_url&&
               
                 
                 <iframe src={`https://${public_url}`}
@@ -56,9 +66,8 @@ const VirtulMatchine = () => {
                 >
                     </iframe>
             }                        */}
-
-        </div>
-    );
-}
+    </div>
+  );
+};
 
 export default VirtulMatchine;
