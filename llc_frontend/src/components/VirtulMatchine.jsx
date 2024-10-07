e062f381643ddea6004943a194a8e55ee290ad2a
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2024-10-07 19:04:12 +0530

    Iframe added.

--- llc_frontend/src/components/VirtulMatchine.jsx
+++ llc_frontend/src/components/VirtulMatchine.jsx
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 import axios from "axios";
 import { useLocation } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
+import "../style/virtual_machine.css";
+import { Typography } from "@mui/material";
 
 const VirtulMatchine = () => {
   const { state } = useLocation();
@@ -9,63 +11,66 @@ const VirtulMatchine = () => {
   const [public_url, updatePublic_url] = useState("");
   const { user, getAccessTokenSilently } = useAuth0();
   useEffect(() => {
-    getAccessTokenSilently()
-      .then((accessToken) => {
-        let owner = user?.nickname;
-        if (!owner) {
-          alert("You Must Logging");
-          return;
-        }
-        let payload = {
-          owner,
-          container_name,
-        };
+    getAccessTokenSilently().then((accessToken) => {
+      let owner = user?.nickname;
+      if (!owner) {
+        //alert("You Must Logging");
+        return;
+      }
+      let payload = {
+        owner,
+        container_name,
+      };
 
-        axios
-          .get(
-            `${import.meta.env.VITE_BACKEND_URL}/launch/`,
+      axios
+        .get(
+          `${import.meta.env.VITE_BACKEND_URL}/launch/`,
 
-            {
-              params: { ...payload },
-              headers: {
-                Authorization: `Bearer ${accessToken}`,
-                "Content-Type": "application/json",
-              },
-            }
-          )
-          .then((res) => {
-            updatePublic_url(res.data.public_url);
-          })
-          .catch((error) => {
-            console.error("Error fetching deployed projects", error);
-          });
-      });
-  }, [container_name]); // Depend on container_name
+          {
+            params: { ...payload },
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+              "Content-Type": "application/json",
+            },
+          }
+        )
+        .then((res) => {
+          updatePublic_url(res.data.public_url);
+        })
+        .catch((error) => {
+          console.error("Error fetching deployed projects", error);
+        });
+    });
+  }, [user, container_name]); // Depend on container_name
 
   // Navigate to the public URL when it's updated
   useEffect(() => {
     if (public_url) {
       //console.log(public_url)
       //console.log(`https://${public_url}`)
-      window.location.href = "https://" + public_url; // Redirect to the public URL
+      //window.location.href = "https://" + public_url; // Redirect to the public URL
     }
   }, [public_url]);
 
   return (
     <div>
       {/* You can add additional UI elements here if needed */}
-      <h1>Launching Virtual Machine...</h1>
-      <h2> It May take some time</h2>
-
-      {/* {public_url&&
-              
-                
-                <iframe src={`https://${public_url}`}
-                    height={500}
-                    width={500}
-                >
-                    </iframe>
-            }                        */}
+      {!public_url && (
+        <>
+          <h1>Launching Virtual Machine...</h1>
+          <h2> It May take some time</h2>
+        </>
+      )}
+      <div className="iframe-cont">
+        <Typography variant="h4">Apine OS</Typography>
+        {public_url && (
+          <iframe
+            src={`https://${public_url}`}
+            height="800"
+            width="1000"
+          ></iframe>
+        )}
+      </div>
     </div>
   );
 };
