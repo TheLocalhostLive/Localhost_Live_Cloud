5990ce120d00412d4e85575a8e200112a68adf0b
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2024-10-25 22:14:26 +0530

    CustomAppbar: Change menu style.

--- llc_frontend/src/components/ui_components/CustomAppBar.jsx
+++ llc_frontend/src/components/ui_components/CustomAppBar.jsx
@@ -67,43 +67,18 @@ export default function CustomAppBar() {
           </IconButton>
         )}
         <Menu
-          sx={{
-            mt: "45px",
-            "& .MuiPaper-root": {
-              borderRadius: "12px", // Rounded corners
-              boxShadow: "0px 8px 24px rgba(0, 0, 0, 0.15)", // Subtle shadow
-              padding: "10px",
-              backgroundColor: "#fff", // White background
-              transition: "all 0.3s ease-in-out", // Smooth transition
-            },
-            "& .MuiMenuItem-root": {
-              borderRadius: "8px", // Rounded items
-              transition: "background 0.3s ease", // Smooth hover effect
-              "&:hover": {
-                backgroundColor: "#f0f0f0", // Hover effect
-                transform: "scale(1.02)", // Slight scaling
-              },
-            },
-          }}
           id="menu-appbar"
           anchorEl={anchorElUser}
-          anchorOrigin={{
-            vertical: "top",
-            horizontal: "right",
-          }}
           keepMounted
-          transformOrigin={{
-            vertical: "top",
-            horizontal: "right",
-          }}
           open={Boolean(anchorElUser)}
           onClose={() => handleCloseUserMenu()}
         >
           {/* Close Button */}
-          <MenuItem disableGutters sx={{ justifyContent: "flex-end" }}>
+          <MenuItem  sx={{justifyContent: "flex-end"}}>
             <IconButton
               edge="end"
               color="inherit"
+              
               onClick={() => handleCloseUserMenu()}
             >
               <CloseIcon />
@@ -115,13 +90,6 @@ export default function CustomAppBar() {
               id={setting.name}
               key={setting.name}
               onClick={() => handleCloseUserMenu(setting.callback)}
-              sx={{
-                fontWeight: "500", // Slightly bold text
-                padding: "10px 20px", // Spacing for better UI
-                "&:not(:last-child)": {
-                  mb: 1, // Margin between menu items
-                },
-              }}
             >
               <Typography sx={{ textAlign: "center" }}>
                 {setting.name}
