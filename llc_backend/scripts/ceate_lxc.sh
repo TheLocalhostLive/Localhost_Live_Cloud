92ef4eaf93d778f017ed73845a9ea674e91af429
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2024-10-29 00:45:48 +0530

    create_lxc: Update storage & network name to server.

--- llc_backend/scripts/ceate_lxc.sh
+++ llc_backend/scripts/ceate_lxc.sh
@@ -2,7 +2,7 @@
 #!/bin/sh
 
 # Launch the container
-sudo incus launch images:alpine/3.20 "$1" --storage incus-storage --network incus-network 
+sudo incus launch images:alpine/3.20 "$1" --storage server-storage --network server-network 
 
 # Run a command to update the repositories and install ttyd
 sudo incus exec "$1" -- sh -c "
