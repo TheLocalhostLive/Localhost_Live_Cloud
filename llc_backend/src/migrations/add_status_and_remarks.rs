3670530e17d8bb8761e83b62d17555694b05fa6a
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2024-12-03 11:31:10 +0530

    Add generic format for migration fn.
    It is done so that this functions can be called dynamically from the main module.

--- llc_backend/src/migrations/add_status_and_remarks.rs
+++ llc_backend/src/migrations/add_status_and_remarks.rs
@@ -1,23 +1,29 @@
-use crate::Applications;
 use mongodb::{bson::doc, Database};
+use std::future::Future;
+use std::pin::Pin;
 
 
 
-pub async fn run(db: &Database) {
-    let apps_collection = db.collection::<Applications>("applications");
-    let update_result = apps_collection
-        .update_many(
-            doc! {},
-            doc! {  "$set": {
-                "status": "HEALTHY",
-                "remarks": "ALL STEPS SUCCESSFULLY EXECUTED"}
-            },
-        )
-        .await
-        .unwrap();
 
-    println!(
-        "Matched: {}, Modified: {}",
-        update_result.matched_count, update_result.modified_count
-    );
+pub fn run(db: &Database) -> Pin<Box<dyn Future<Output = ()> + Send>> {
+    let db = db.clone(); // Clone the database handle
+    Box::pin(async move {
+        let apps_collection = db.collection::<mongodb::bson::Document>("applications");
+        let update_result = apps_collection
+            .update_many(
+                doc! {},
+                doc! { "$set": {
+                    "status": "HEALTHY",
+                    "remarks": "ALL STEPS SUCCESSFULLY EXECUTED"
+                }},
+            )
+            .await
+            .expect("Update failed");
+
+        println!(
+            "Migration 'add_status_and_remarks' applied successfully. \
+             Matched: {}, Modified: {}",
+            update_result.matched_count, update_result.modified_count
+        );
+    })
 }
